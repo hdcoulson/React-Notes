57bfa5a2703f74a5529f3935ede1c26774776eb6
Harry Coulson <coulson.mail@me.com>
2017-05-05 12:42:50 -0700

    Update notes.js

--- client/notes.js
+++ client/notes.js
@@ -2,7 +2,7 @@ var React = require('react')
 
 function Notes(props) {
   const $notes = props.map(function (note, index) {
-    const $note = React.createElement('li', {key: index}, note)
+    const $note = React.createElement('li', {key: index}, note.text)
     return $note
   })
   const $renderedList = React.createElement('ul', null, $notes)
